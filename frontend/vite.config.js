fea44b22442a80abfb40e744eb0d69096f80db48
Ravindu <it22284198@my.sliit.lk>
2025-09-24 13:43:37 +0530

    vite.confug + package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    cors: {
+      origin: [
+        'http://localhost:5173', 
+        'http://127.0.0.1:5173', 
+        'http://localhost:3000'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true
+    }
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn']
+      },
+      format: {
+        comments: false
+      },
+      mangle: {
+        toplevel: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['axios', 'jspdf']
+        }
+      }
+    },
+    sourcemap: false
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+    legalComments: 'none'
+  },
+  optimizeDeps: {
+    exclude: ['node_modules']
+  }
 })
